c59422a7f52d7f36f161e32b2f33e79a3096b5a3
gkenji110 <gusavo.terada01@gmail.com>
2025-09-30 14:15:02 -0300

    feat: created footer and header components, created home page, instaled tailwind and react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Home } from "./pages/home"
+
 function App() {
 
 
   return (
-    <>
-    </>
+    <BrowserRouter>
+        <Routes>
+          <Route>
+            <Route path="/" element={<Home />} />
+            <Route index element={<Home />} />
+          </Route>
+        </Routes>
+    </BrowserRouter>
   )
 }
 
